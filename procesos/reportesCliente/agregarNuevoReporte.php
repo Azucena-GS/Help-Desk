a7cad12669b33e3a7c40d600cad1d19eb250409f
Azucena-GS <granadosazucena396@gmail.com>
2026-03-18 00:03:25 -0600

    Programacion modulo reportes cliente

--- procesos/reportesCliente/agregarNuevoReporte.php
+++ procesos/reportesCliente/agregarNuevoReporte.php
@@ -1,10 +1,10 @@
 <?php
     session_start();
-    $idUsuario=$_SESSION['usuario']['id'];
+    $idUsuario = $_SESSION['usuario']['id'];
     $datos= array(
-        'idEquipo'=>$_POST['idEquipo'],
-        'problema'=>$_POST['problema'],
-        'idUsuario'=>$idUsuario
+        'idEquipo' => $_POST['idEquipo'],
+        'problema' => $_POST['problema'],
+        'idUsuario' => $idUsuario
     );
 
     include "../../clases/Reportes.php";
