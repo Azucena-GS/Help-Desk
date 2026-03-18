921b2475d71735c8dc71326b97fb2ee5dbfa3905
Azucena-GS <granadosazucena396@gmail.com>
2026-03-17 23:50:21 -0600

    Maquetacion modulo reportes

--- vistas/misReportes.php
+++ vistas/misReportes.php
@@ -1,4 +1,5 @@
-<?php include_once 'header.php'; 
+<?php 
+include 'header.php'; 
   if(isset($_SESSION['usuario']) && $_SESSION['usuario']['rol']== 1){  
     include '../clases/Conexion.php';
       $con= new Conexion();
@@ -8,7 +9,7 @@
   <div class="container">
     <div class="card border-0 shadow my-5">
       <div class="card-body p-5">
-        <h1 class="fw-light">Reportes de clientes</h1>
+        <h1 class="fw-light">Reportes de cliente</h1>
         <p class="lead">
           <button class="btn btn-primary" data-toggle="modal" data-target="#modalCrearReporte">
             Crear Reporte
@@ -22,7 +23,7 @@
 
 <?php 
 include 'reportesClientes/modalCrearReporte.php';
-include_once 'footer.php';
+include 'footer.php';
 ?>
 <script src="../public/js/reportesCliente/reportesCliente.js"></script>
 
