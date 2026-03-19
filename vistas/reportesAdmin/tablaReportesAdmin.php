568d241aa36241267c87f7e23f32ff0b280a33dc
Azucena-GS <granadosazucena396@gmail.com>
2026-03-19 02:04:33 -0600

    Cambio :)

--- vistas/reportesAdmin/tablaReportesAdmin.php
+++ vistas/reportesAdmin/tablaReportesAdmin.php
@@ -1,42 +1,42 @@
-
 <?php
     session_start();
-    include '../../clases/Conexion.php';   
+    include "../../clases/Conexion.php";
     $con = new Conexion();
     $conexion = $con->conectar();
-    $idUsuario=$_SESSION['usuario']['id'];
-    $contador=1;
-    $sql="SELECT 
-            reporte.id_reporte AS idReporte,
-            reporte.id_usuario AS idUsuario,
-            CONCAT(persona.paterno,
-                    ' ',
-                    persona.materno,
-                    ' ',
-                    persona.nombre) AS nombrePersona,
-            equipo.id_equipo AS idEquipo,
-            equipo.nombre AS nombreEquipo,
-            reporte.descripcion_problema AS problema,
-            reporte.estatus AS estatus,
-            reporte.solucion_problema AS solucion,
-            reporte.fecha AS fecha
-        FROM
-            t_reportes AS reporte
-                INNER JOIN
-            t_usuarios AS usuario ON reporte.id_usuario = usuario.id_usuario
-                INNER JOIN
-            t_persona AS persona ON usuario.id_persona = persona.id_persona
-                INNER JOIN
-            t_cat_equipo AS equipo ON reporte.id_equipo = equipo.id_equipo
-            ORDER BY reporte.fecha DESC";
-
-     $respuesta = mysqli_query($conexion, $sql);
+    $idUsuario = $_SESSION['usuario']['id'];
+    $contador = 1;
+    $sql = "SELECT 
+                reporte.id_reporte AS idReporte,
+                reporte.id_usuario AS idUsuario,
+                CONCAT(persona.paterno,
+                        ' ',
+                        persona.materno,
+                        ' ',
+                        persona.nombre) AS nombrePersona,
+                equipo.id_equipo AS idEquipo,
+                equipo.nombre AS nombreEquipo,
+                reporte.descripcion_problema AS problema,
+                reporte.estatus AS estatus,
+                reporte.solucion_problema AS solucion,
+                reporte.fecha AS fecha
+            FROM
+                t_reportes AS reporte
+                    INNER JOIN
+                t_usuarios AS usuario ON reporte.id_usuario = usuario.id_usuario
+                    INNER JOIN
+                t_persona AS persona ON usuario.id_persona = persona.id_persona
+                    INNER JOIN
+                t_cat_equipo AS equipo ON reporte.id_equipo = equipo.id_equipo 
+                ORDER BY reporte.fecha DESC";
+                
+    $respuesta = mysqli_query($conexion, $sql);
 ?>
-<table class="table table-sm dt-responsive nowrap  table-bordered"
-        id="tablaReporteAdminDataTable"  style="width:100%">
+
+<table class="table table-sm table-bordered dt-responsive nowrap" 
+        style="width:100%" id="tablaReportesAdminDataTable">
     <thead>
         <th>#</th>
-        <th>Nombre</th>
+        <th>Persona</th>
         <th>Dispositivo</th>
         <th>Fecha</th>
         <th>Descripcion</th>
@@ -45,100 +45,84 @@
         <th>Eliminar</th>
     </thead>
     <tbody>
-    <?php 
-        while($mostrar=mysqli_fetch_array($respuesta)){  
-    ?>
+    <?php while($mostrar = mysqli_fetch_array($respuesta)) {  ?>
         <tr>
-            <td><?php echo $contador++;?></td>
-            <td><?php echo $mostrar['nombrePersona']?></td>
-            <td><?php echo $mostrar['nombreEquipo']?></td>
-            <td><?php echo $mostrar['fecha']?></td>
-            <td><?php echo $mostrar['problema']?></td>
+            <td> <?php echo $contador++; ?> </td>
+            <td><?php echo $mostrar['nombrePersona'];?></td>
+            <td><?php echo $mostrar['nombreEquipo'];?></td>
+            <td><?php echo $mostrar['fecha'];?></td>
+            <td><?php echo $mostrar['problema'];?></td>
             <td>
                 <?php
-                $estatus= $mostrar['estatus'];
-                $cadenaEstatus='<span class="badge badge-success"> Abierto </span>';
-
-                if($estatus==0){
-                    $cadenaEstatus='<span class="badge badge-danger"> Cerrado </span>';
-                }
-                echo $cadenaEstatus;
+                    $estatus = $mostrar['estatus'];
+                    $cadenaEstatus = '<span class="badge badge-danger">Abierto</span>';
+                    if ($estatus == 0) {
+                        $cadenaEstatus = '<span class="badge badge-success">Cerrado</span>';
+                    }
+                    echo $cadenaEstatus;
                 ?>
             </td>
             <td>
                 <button class="btn btn-info btn-sm" 
-                        data-toggle="modal" data-target="#modalAgregarSolucionReporte"
-                        onclick="obtenerDatosSolucion('<?php echo $mostrar['idReporte']; ?>')">
-                        Solucion
-                    </button>
-                <?php echo $mostrar['solucion']?></td>
+                        onclick="obtenerDatosSolucion('<?php echo $mostrar['idReporte'];?>')"
+                        data-toggle="modal" data-target="#modalAgregarSolucionReporte">
+                    Solucion
+                </button>
+                <?php echo $mostrar['solucion'];?>
+            </td>
             <td>
                 <?php
-                if($mostrar['solucion']==""){
+                    if ($mostrar['solucion'] == "") {
                 ?>
-                <button class="btn btn-danger btn-sm"
-                    onclick="eliminarReporteAdmin(<?php echo $mostrar['idReporte']?>)">
-                    Eliminar
-                </button>
-
+                        <button class="btn btn-danger btn-sm" 
+                            onclick="eliminarReporteAdmin(<?php echo $mostrar['idReporte'] ?>)">
+                            Eliminar
+                        </button>
                 <?php
-                }
+                    }
                 ?>
             </td>
-
         </tr>
-    <?php
-        }
-    ?>    
+    <?php } ?>
     </tbody>
 </table>
 
 <script>
     $(document).ready(function(){
-        $('#tablaReporteClienteDataTable').DataTable({
-            language :{
-                 url : "../public/datatable/es_es.json"
+        $('#tablaReportesAdminDataTable').DataTable({
+            language : {
+                url : "../public/datatable/es_es.json"
             },
-            
             dom: 'Bfrtip',
-            buttons:{
-                buttons: [
-                    {
-                        extend:'copy', 
-                        className: 'btn btn-outline-info', 
-                        text:'<i class="far fa-copy"></i> Copiar'
-                    },   
-                    
-                    {
-                        extend:'csv', 
-                        className: 'btn btn-outline-primary', 
-                        text:'<i class="fas fa-file-csv"></i> CSV'
-                    },   
-
-
-                    {
-                        extend:'excel', 
-                        className: 'btn btn-outline-success', 
-                        text:'<i class="fas fa-file-excel"></i> Excel'
-                    },   
-
-                    {
-                        extend:'pdf', 
-                        className: 'btn btn-outline-danger', 
-                        text:'<i class="fas fa-file-pdf"></i> PDF'
-                    },   
+            buttons : {
+                buttons : [
+                    {   
+                        extend : 'copy', 
+                        className : 'btn btn-outline-info', 
+                        text : '<i class="far fa-copy"></i> Copiar' 
+                    },
+                    {   
+                        extend : 'csv', 
+                        className : 'btn btn-outline-primary', 
+                        text : '<li class="fas fa-file-csv"></li> CSV' 
+                    },
+                    {   
+                        extend : 'excel', 
+                        className : 'btn btn-outline-success', 
+                        text : '<i class="fas fa-file-excel"></i> XLS' 
+                    },
+                    {   
+                        extend : 'pdf', 
+                        className : 'btn btn-outline-danger', 
+                        text : '<i class="fas fa-file-pdf"></i> PDF' 
+                    },
                 ],
-
-
-                dom:{
-                    button: {
+                dom : {
+                    button : {
                         className : 'btn'
-
                     }
                 }
             }
-
         });
-    });
-
+    })
 </script>
