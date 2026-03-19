34f5b227bc20f3b0af78ab08640c8b1a2e8e6a9d
Azucena-GS <granadosazucena396@gmail.com>
2026-03-18 19:25:33 -0600

    Add files via upload

---
+++ public/js/inicio/actualizarPersonales.js
@@ -0,0 +1,42 @@
+function actualizarDatosPersonales(){
+
+    $.ajax({
+        type: "POST",
+        data: $('#frmActualizarDatosPersonales').serialize(),
+        url: "../procesos/inicio/actualizarPersonales.php",
+        success: function(respuesta){
+            respuesta=respuesta.trim();
+
+            if(respuesta==1){
+                
+                Swal.fire(":D","Actualizo sus datos con exito","success");
+                location.reload();
+            }else{
+                Swal.fire(":(","No se pudo realizar la actualizacion, error:" + respuesta ,"error");
+            }
+        }
+    });
+
+    return false;
+  
+}
+
+function obtenerDatosPersonalesInicio(idUsuario){
+    $.ajax({
+        type: "POST",
+        data: { idUsuario: idUsuario },
+        url: "../procesos/usuarios/crud/obtenerDatosUsuario.php",
+        success: function(respuesta){
+
+            respuesta = jQuery.parseJSON(respuesta);
+            $('#paternoInicio').val(respuesta['paterno']);
+            $('#maternoInicio').val(respuesta['materno']);
+            $('#nombreInicio').val(respuesta['nombrePersona']);
+            $('#telefonoInicio').val(respuesta['telefono']);
+            $('#correoInicio').val(respuesta['correo']);
+            $('#fechaInicio').val(respuesta['fechaNacimiento']);
+    
+
+        }
+    });
+}
